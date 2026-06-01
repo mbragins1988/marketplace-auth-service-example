f9aaea696e059dd0c352011d07f4ccb910e187cb
Михаил Брагин <mbragins@mail.ru>
2026-06-01 13:41:12 +0500

    POSTGRES_CONNECTION_STRING chenged

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,13 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env",
+                                      env_file_encoding="utf-8",
+                                      extra="ignore")
 
-    database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/auth_db"
+    database_url: str = os.getenv("POSTGRES_CONNECTION_STRING")
     jwt_secret: str = "change-me"
     jwt_algorithm: str = "HS256"
     jwt_expire_hours: int = 24
