c31e2fd3baba69809facf478a6a45caa1433046a
Михаил Брагин <mbragins@mail.ru>
2026-06-01 17:19:25 +0500

    alembic fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+<<<<<<< HEAD
 export PATH="/root/.local/bin:$PATH"
 
 cd /app
 
 echo "Applying database migrations..."
+=======
+cd /app
+
+echo "Applying database migrations"
+>>>>>>> 854fa84 (alembic fix)
 uv run alembic upgrade head
 
 echo "Starting FastAPI application..."
