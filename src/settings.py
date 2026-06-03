bf98b2aa5451c670f5b7adb5b4ab7a4d45275d4b
Mikhail <mbragins@mail.ru>
2026-06-03 11:49:36 +0500

    ruff check

--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,12 @@
 import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env",
-                                      env_file_encoding="utf-8",
-                                      extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
     jwt_secret: str = "change-me"
     jwt_algorithm: str = "HS256"
@@ -16,7 +17,7 @@ class Settings(BaseSettings):
     def database_url(self) -> str:
         # Получаем переменную из окружения
         pg_conn = os.getenv("POSTGRES_CONNECTION_STRING")
-        
+
         if pg_conn:
             # Если строка начинается с postgres:// или postgresql://
             if pg_conn.startswith("postgres://"):
@@ -24,6 +25,6 @@ class Settings(BaseSettings):
             elif pg_conn.startswith("postgresql://") and "+asyncpg" not in pg_conn:
                 pg_conn = pg_conn.replace("postgresql://", "postgresql+asyncpg://", 1)
             return pg_conn
-        
+
         # Fallback для локальной разработки
         return "postgresql+asyncpg://postgres:postgres@localhost:5433/auth_db"
